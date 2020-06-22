ec6377b5aaa7090869684bdc8233e0933e666d54
Chihyu <chihyu10@gmail.com>
2020-06-22 10:02:50 +0200

    Boilerplate

---
+++ HTTPClientDemoAppClean/HttpClientDemo/Models/WeatherForecastModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpClientDemo.Models
+{
+    public class WeatherForecastModel
+        {
+            /*public DayForecastModel[] Consolidated_weather { get; set; }
+            public DateTime Sun_rise { get; set; } // ideally sunRise -> SunRise
+            public DateTime Sun_set { get; set; }
+            public string Title { get; set; }
+            public string Timezone { get; set; }
+        }
+
+        public class Parent
+        {
+            public string title { get; set; }
+            public string location_type { get; set; }
+            public int woeid { get; set; }
+            public string latt_long { get; set; }*/
+        }
+}
